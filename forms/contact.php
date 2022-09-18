5067176caa727a09e4e6cbf6c1360fe00ddd854a
Tomas <tomas@MacBook-Pro-van-Tomas.local>
2022-09-18 13:19:08 +0200

    try another form

--- forms/contact.php
+++ forms/contact.php
@@ -1,14 +1,21 @@
-<?php
-$mail_to_send_to = "tomas.fleiderman@gmail.com";
+<?
+$mail_to_send_to = "name@anydomain.tld";
 $from_email = "from@yourdomain.tld";
-$name=$_REQUEST["name"];
-$subject= "Message subject";
-$email = $_REQUEST["email"] ;
-$message= "\r\n" . "Name: $name" . "\r\n"; 
-$message = $message.$_REQUEST["message"] . "\r\n" ;
-$headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-$res = mail( $mail_to_send_to, $subject, $message, $headers );
-//var_dump($res);
-?>
+$sendflag = $_REQUEST['sendflag'];    
+$name=$_REQUEST['name'];
+if ( $sendflag == "send" )
+        {
+                $subject= "Message subject";
+                $email = $_REQUEST['email'] ;
+                $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
+                $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
+                $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
+                $a = mail( $mail_to_send_to, $subject, $message, $headers );
+                if ($a)
+                {
+                     print("Message was sent, you can send another one");
+                } else {
+                     print("Message wasn't sent, please check that you have changed emails in the bottom");
+                }
+        }
+        ?>
